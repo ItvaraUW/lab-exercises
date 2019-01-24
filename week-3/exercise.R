7c8f5dd76263553f92fc8204345b93b4fea403bd
Sean Le <itvara@uw.edu>
2019-01-24 15:13:21 -0800

    Finished week 3 exercises.

--- week-3/exercise.R
+++ week-3/exercise.R
@@ -1,15 +1,30 @@
 iris_df <- data.frame(iris)
+View(iris_df)
 
 # Select all values of data frame for Sepal.length
+View(iris_df$Sepal.Length)
 
 # Select only rows of Virginica flowers
 
+virginica_iris <- iris_df[iris_df$Species == "virginica",]
+
 # Select only the sepal legnth of the Virginica flowers
+virginica_iris[, "Sepal.Length"]
+virginica_iris$Sepal.Length
 
 # Select rows where Petal.Length > 4.0
+iris_df[iris_df$Petal.Length > 4.0, ]
 
 # Add a column to the dataframe indicating whether Petal.Length > 4.0
+iris_df$if_petal_greater_than_4 <- iris_df$Petal.Length > 4.0
 
 # Find the species type of the flower that has the longest Sepal length
+max(iris_df$Sepal.Length)
+iris_df[iris_df$Sepal.Length == max(iris_df$Sepal.Length), "Species"]
 
 # Select rows where species type is 'setosa' and Sepal.Width > 3.0
+setosa_df <- iris_df[iris_df$Species == "setosa",]
+setosa_df[setosa_df$Sepal.Width > 3.0, ]
+
+# One line solution
+View(iris[((iris$Species == "setosa") & (iris$Sepal.Width > 3.0)), ])
--- week-3/week-3.R
+++ week-3/week-3.R
@@ -4,28 +4,33 @@
 event_capacity <- c(60, 50, '100', 100)
 
 # Fix the possible error in a new variable event_capacity_new
+event_capacity_new <- c(60, 50, 100, 100)
 
 # Create a vector of event names for each event 
-event_names <- 
+event_names <- c("Event 1", "Event 2", "Event 3", "Event 4")
   
 # Fix the error in creating this dataframe
 events <- data.frame(event_names, event_cost, event_capacity)
+events <- data.frame(event_names, event_capacity_new)
 
 # Check the event_capacity column
 
 events$event_capacity
+events$event_capacity_new
 
 # Try with event_capacity_new column
 events <- data.frame(event_names, event_capacity_new)
 
 # Are there any differences between event_capacity and event_capacity)_new?
 # Answer here
+# When checking by looking at the values stored, no there looks like there is
+# no difference but there is a single value that is stored as a string instead of an integer.
 
 # Fix an error of adding columns to the dataframe
-events.event_attendance <- c(54, 43, 85, 90)
+events$event_attendance <- c(54, 43, 85, 90)
 
 # Add a column called seats_left in the events dataframe
-events$seats_left <-
-  
-  
+events$seats_left <- events$event_capacity_new - events$event_attendance
+
+View(events)
   
